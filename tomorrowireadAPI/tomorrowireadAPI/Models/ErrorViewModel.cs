6b355fdd0703328ffe6fefa8235c93ba2be8028b
Gabriel <gabrielreech@gmail.com>
2023-05-10 19:45:53 +0100

    create project

---
+++ tomorrowireadAPI/tomorrowireadAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tomorrowireadAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
